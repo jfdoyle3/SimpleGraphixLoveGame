4b7794b91b1bc20a11b0e33121e1bda1e50847d7
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2024-02-09 16:59:58 -0500

    added more function to NPC.

--- objects/npc.lua
+++ objects/npc.lua
@@ -15,3 +15,21 @@ function Npc:new()
 	self.y=20
 	self.speed=500
 end
+
+function Npc:update(dt)
+    self.x = self.x + self.speed * dt
+
+    local window_width = love.graphics.getWidth()
+
+    if self.x < 0 then
+        self.x = 0
+        self.speed = -self.speed
+    elseif self.x + self.width > window_width then
+        self.x = window_width - self.width
+        self.speed = -self.speed
+    end
+end
+
+function Npc:draw()
+    love.graphics.draw(self.image, self.x, self.y)
+end
