8fb3ea0dd80cfe187755f13fa547f103af03adc3
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2024-03-08 12:15:46 -0500

    trying to get accelerometer working

--- main.lua
+++ main.lua
@@ -1,27 +1,18 @@
+local joystick, position, speed
 function love.load()
-    local joysticks = love.joystick.getJoysticks()
-    joystick = joysticks[1]
+   joystick = love.joystick.getJoysticks()[1]
+   position = {x = 0, y = 0}
+   speed = 10
 
-    position = {x = 400, y = 300}
-    speed = 300
 end
 
 function love.update(dt)
-    if not joystick then return end
-
-    if joystick:isGamepadDown("dpleft") then
-        position.x = position.x - speed * dt
-    elseif joystick:isGamepadDown("dpright") then
-        position.x = position.x + speed * dt
-    end
-
-    if joystick:isGamepadDown("dpup") then
-        position.y = position.y - speed * dt
-    elseif joystick:isGamepadDown("dpdown") then
-        position.y = position.y + speed * dt
-    end
+   if joystick then
+     local axis1, axis2 = joystick:getAxes()
+     position.x, position.y = position.x + axis1 * speed * dt, position.y + axis2 * speed * dt
+  end
 end
 
 function love.draw()
-    love.graphics.circle("fill", position.x, position.y, 50)
+   love.graphics.circle("fill", position.x, position.y, 50)
 end
