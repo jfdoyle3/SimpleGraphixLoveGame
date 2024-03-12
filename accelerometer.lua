ed11871296624b6ae55627fd36f1df92487391b6
Jim Doyle <indellibleink@gmail.com>
2024-03-12 07:29:31 -0400

    updated accelerometer.lua

--- accelerometer.lua
+++ accelerometer.lua
@@ -7,6 +7,16 @@ for i in ipairs(joysticks) do
 looped = looped + 1
 end
 ]]
+
+--[[
+      joysticks is a table
+   
+   There are 2 joysticks on phone
+   1 - joystick
+   2 - accelerometer
+   
+   using # picks the last element in the table
+]]
 joystick = joysticks[#joysticks]
 position = {
   x = 0, y = 0
@@ -22,6 +32,20 @@ position.x, position.y = position.x + axis1 * speed * dt, position.y + axis2 * s
 end
 end
 
+-- Prints a table to console
+function dump(o)
+   if type(o) == 'table' then
+      local s = '{ '
+      for k,v in pairs(o) do
+         if type(k) ~= 'number' then k = '"'..k..'"' end
+         s = s .. '['..k..'] = ' .. dump(v) .. ','
+      end
+      return s .. '} '
+   else
+      return tostring(o)
+   end
+end
 function love.draw()
+  love.graphics.print(dump(joysticks),100,100)
 love.graphics.circle("fill", position.x, position.y, 50)
 end
